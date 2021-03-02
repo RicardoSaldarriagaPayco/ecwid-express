9fa3fb8699fbb85510ba7af92a090bb81f50287e
pruebasepayco <ricardo.saldarriaga@payco.co>
2021-03-02 15:54:09 -0500

    add router and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+//Routing
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+//Components
+import Login from './components/home/Login';
+import Payco from './components/checkout/payco';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <header className="App-header">
+          
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/checkout"}>Checkout</Link>
+        </header>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/checkout" component={Payco} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ client/src/components/checkout/payco.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Payco(){
+    return(
+        <h1>checkout</h1>
+    )
+}   
+export default Payco;
